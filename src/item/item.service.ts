66d5efbf65454f3339c0d8364b89eb04f5696b70
Ryo Shibayama <j02521@gmail.com>
2021-07-18 02:05:14 +0900

    Add injector

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { Item } from "src/entities/item.entity";
+import { Repository } from "typeorm";
+import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
-export class ItemService {}
+export class ItemService {
+    constructor(
+        @InjectRepository(Item)
+        private readonly itemRepository: Repository<item>
+    ) { }
+}
