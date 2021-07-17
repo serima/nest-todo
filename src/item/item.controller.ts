76ea0fccbb6cdb59a6e3a7435bf5b425df89adbf
Ryo Shibayama <j02521@gmail.com>
2021-07-18 02:22:54 +0900

    Add some methods

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Body,
+    Param,
+} from '@nestjs/common';
+import { ItemService } from './item.service';
+import { Item } from "../entities/item.entity";
+import { CreateItemDTO } from './item.dto';
+import { InsertResult, UpdateResult, DeleteResult } from 'typeorm';
 
 @Controller('item')
-export class ItemController {}
+export class ItemController {
+    constructor(private readonly service: ItemService) { }
+
+    // itemのURIへのGETメソッドで全件取得。serviceのfindAll()を実行
+    @Get()
+    async getItemList(): Promise<Item[]> {
+        return await this.service.findAll();
+    }
+
+    // itemのURIへのPOSTメソッドでデータ新規登録
+    @Post()
+    async addItem(@Body() item: CreateItemDTO): Promise<InsertResult> {
+        return await this.service.create(item);
+    }
+
+    // item/id番号のURIへのGETメソッドでid指定で1件データ取得
+    @Get(":id")
+    async getItem(@Param("id") id: string): Promise<Item> {
+        return await this.service.find(Number(id));
+    }
+}
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from "src/entities/item.entity";
-import { Repository } from "typeorm";
+import { Repository, InsertResult, UpdateResult, DeleteResult } from "typeorm";
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateItemDTO } from './item.dto';
 @Injectable()
 export class ItemService {
     constructor(
         @InjectRepository(Item)
-        private readonly itemRepository: Repository<item>
+        private readonly itemRepository: Repository<Item>
     ) { }
+
+    // テーブルの全データを取得する
+    async findAll(): Promise<Item[]> {
+        return await this.itemRepository.find();
+    }
+
+    // テーブルにアイテムを追加する
+    async create(item: CreateItemDTO): Promise<InsertResult> {
+        return await this.itemRepository.insert(item);
+    }
+
+    // idを指定してテーブルから1件のデータを取得する
+    async find(id: number): Promise<Item> | null {
+        return await this.itemRepository.findOne({ id: id });
+    }
+
+    // idを指定してテーブルのデータを更新する
+    async update(id: number, item): Promise<UpdateResult> {
+        return await this.itemRepository.update(id, item);
+    }
+
+    // idを指定してテーブルのデータを削除する
+    async delete(id: number): Promise<DeleteResult> {
+        return await this.itemRepository.delete(id);
+    }
+
 }
